58bc8766939b3abc17de6e753aa97e71ae40329b
matinahuel <matiasnahuel2396@gmail.com>
2021-05-12 14:52:32 -0300

    mi primer commit

---
+++ git.js
@@ -0,0 +1,11 @@
+//git init                                                       //inicia un repositorio en la carpeta seleccionada
+//rm -rf .git                                                   // con este comando se borra el git init
+// git config user.name "nombreDeUsuario"                      //agrega tu usuario
+// git config user.name                                       // verificas el usuario
+// git config user.email "nombre@email.com"                  //agrega tu mail
+//git config user.email                                     //verifica el mail
+//git config --global user.name username                   //agrega tu user global en los archivos
+//git config --global user.mail usermail                  //agrega tu mail global en los archivos
+
+//git add . //para guardar en el repositorio los archivos sin seguimiento
+//git commit -m"mensaje"
